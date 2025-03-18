bc4dd9aad6eb6bbbcdbc88af6a7abd1817cbabfe
Vijay-Nataraj <victoryvijayn@gmail.com>
2025-03-18 12:37:52 +0530

    cors updated

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "vj-freelance-marketplace.netlify.app",
     credentials: true,
   })
 );
